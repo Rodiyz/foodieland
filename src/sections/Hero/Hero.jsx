1df3462c36198d59ea2143bd0659dbab10465b2e
Rodiyz <malinkin54ru@gmail.com>
2025-11-28 13:01:42 +0700

    swiper

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -3,7 +3,34 @@ import './Hero.scss'
 export default () => {
   return (
     <section className="hero" aria-labelledby="hero-title">
-      <div className="hero__inner container">Hero section content</div>
+      <div className="hero__slider swiper" data-js-slider="">
+        <ul className="hero__slider-list swiper-wrapper">
+          <li className="hero__slider-item swiper-slide">
+            <div className="hero__card">
+              1 lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam totam quod doloremque quidem aspernatur necessitatibus illo cum asperiores quae ipsam rerum, quaerat voluptates, deleniti error eveniet commodi magni, suscipit culpa!
+              Accusantium ullam magnam sed, corrupti magni sapiente porro aut quas ad facilis amet accusamus debitis blanditiis expedita dolores necessitatibus deserunt eius vel quo maxime iste? Minus asperiores laudantium placeat repellendus.
+              Quos sunt cum veniam minus nulla sit commodi aut expedita non, saepe quis praesentium beatae eos quod harum perferendis magnam velit? Reprehenderit quibusdam unde rerum ad quam necessitatibus sunt repellendus?
+              Sequi numquam perspiciatis repellat. Vitae totam tenetur cum repellendus odio? Dolore eaque quisquam earum error aspernatur aut porro voluptatem qui! Dolor sint iste ab explicabo consectetur obcaecati adipisci reiciendis maxime?
+            </div>
+          </li>
+          <li className="hero__slider-item swiper-slide">
+            <div className="hero__card">
+              2 lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam totam quod doloremque quidem aspernatur necessitatibus illo cum asperiores quae ipsam rerum, quaerat voluptates, deleniti error eveniet commodi magni, suscipit culpa!
+              Accusantium ullam magnam sed, corrupti magni sapiente porro aut quas ad facilis amet accusamus debitis blanditiis expedita dolores necessitatibus deserunt eius vel quo maxime iste? Minus asperiores laudantium placeat repellendus.
+              Quos sunt cum veniam minus nulla sit commodi aut expedita non, saepe quis praesentium beatae eos quod harum perferendis magnam velit? Reprehenderit quibusdam unde rerum ad quam necessitatibus sunt repellendus?
+              Sequi numquam perspiciatis repellat. Vitae totam tenetur cum repellendus odio? Dolore eaque quisquam earum error aspernatur aut porro voluptatem qui! Dolor sint iste ab explicabo consectetur obcaecati adipisci reiciendis maxime?
+            </div>
+          </li>
+          <li className="hero__slider-item swiper-slide">
+            <div className="hero__card">
+              3 lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam totam quod doloremque quidem aspernatur necessitatibus illo cum asperiores quae ipsam rerum, quaerat voluptates, deleniti error eveniet commodi magni, suscipit culpa!
+              Accusantium ullam magnam sed, corrupti magni sapiente porro aut quas ad facilis amet accusamus debitis blanditiis expedita dolores necessitatibus deserunt eius vel quo maxime iste? Minus asperiores laudantium placeat repellendus.
+              Quos sunt cum veniam minus nulla sit commodi aut expedita non, saepe quis praesentium beatae eos quod harum perferendis magnam velit? Reprehenderit quibusdam unde rerum ad quam necessitatibus sunt repellendus?
+              Sequi numquam perspiciatis repellat. Vitae totam tenetur cum repellendus odio? Dolore eaque quisquam earum error aspernatur aut porro voluptatem qui! Dolor sint iste ab explicabo consectetur obcaecati adipisci reiciendis maxime?
+            </div>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
